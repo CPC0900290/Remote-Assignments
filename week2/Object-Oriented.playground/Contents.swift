b7a652391e8d4d28459bd4f6a5c2ab37b1302150
CPC0900290 <62605033+CPC0900290@users.noreply.github.com>
2024-01-19 17:13:57 +0800

    Creating all of the files of assignments and Completing Part2-Enumeration-Oprionals.playground

---
+++ week2/Object-Oriented.playground/Contents.swift
@@ -0,0 +1,72 @@
+import UIKit
+
+/*:
+ # Part 1: Object-Oriented Swift
+ 1. Declare a class Animal with a property gender and a method eat() . The data type of gender should be enum Gender as below and when you call eat() method, it will print “I eat everything!”
+ */
+ enum Gender {
+ case male
+ case female
+ case undefined
+ }
+// Write your code below.
+ class Animal {
+     init(gender: Gender) {
+         
+     }
+     
+     func eat() {
+         print("I eat everything!")
+     }
+ }
+/*:
+ 2. Declare three classes: Elephant , Tiger , Horse that inherits from Animal and override the eat() method to print what they usually eat.
+ */
+class Elephant: Animal {
+    
+    override func eat() {
+        print("I eat leaves!")
+    }
+}
+class Tiger: Animal {
+    override func eat() {
+        print("I eat Horse!")
+    }
+}
+class Horse: Animal {
+    override func eat() {
+        print("I eat grass!")
+    }
+}
+/*:
+ 3. Declare a class Zoo with a property weeklyHot which means the most popular one in the zoo this week. The codes below can’t work correctly, please find what data type should A be and solve the problem. Note that tiger , elephant , and horse are instances of class Tiger, Elephant, and Horse respectively.
+*/
+ class Zoo {
+ var weeklyHot: Animal
+     init(weeklyHot: Animal) { self.weeklyHot = weeklyHot }
+ }
+let zoo = Zoo(weeklyHot: Tiger(gender: Gender.female))
+ zoo.weeklyHot = Tiger(gender: Gender.undefined)
+ zoo.weeklyHot = Elephant(gender: Gender.undefined)
+ zoo.weeklyHot = Horse(gender: Gender.undefined)
+// Write your code below.
+
+/*:
+ 4. What is an instance? What does Initilizer do in Class and Struct?
+ */
+
+/*:
+ 5. What’s the difference between Struct and Class ?
+ */
+
+/*:
+ 6. What’s the difference between reference type and value type ?
+ */
+
+/*:
+ 7. What’s the difference between instance method and type method ?
+ */
+
+/*:
+ 8. What does self mean in an instance method and a type method respectively?
+ */
---
+++ week2/Part5-Simple-App.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+/*:
+ # Part 5: A Simple App
+ Please implement the app by following the design below (measured in points.)
+ 
+ ## UI Requirements:
+ Label on the top:
+ 
+ Top: 100, Leading: 40, Font size: 16, Font Color: White
+ 
+ Label in the middle:
+ 
+ Top: 100, Leading: 40, Trailing: 40, Font size: 16, Font Color: White
+ 
+ Button:
+ 
+ Bottom: 50, Leading: 40, Trailing: 40, Font size: 16, Font Color: White
+ 
+ 
+ 
+ Functional Requirements:
+ Each time the user hits the button, the background color and text should change randomly. We give the text below. There are 7 sentences in the array. You can choose 7 kinds of color to cooperate with it. For example, you can take the colors of the rainbow as your color set.
+  
+  Hints:
+ 1. RGB color
+ 2. Random number in Swift
+ ```
+ let text=[
+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus.",
+ "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+ "Richard McClintock, a Latin professor at Hampden-Sydney College in ",
+ "looked up one of the more obscure Latin words, consectetur",
+ "from a Lorem Ipsum passage, and going through the cities of the word",
+ "This book is a treatise on the theory of ethics, very popular during the.",
+ "The first line of Lorem Ipsum, Lorem ipsum dolor sit amet..",
+ ]
+ ```
+*/
