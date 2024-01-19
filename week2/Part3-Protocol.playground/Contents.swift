bb8a8f8ee39117171ae4104b88ba08abace9751e
CPC0900290 <62605033+CPC0900290@users.noreply.github.com>
2024-01-19 21:42:33 +0800

    Completing Part3-Protocol.playground

--- week2/Part3-Protocol.playground/Contents.swift
+++ week2/Part3-Protocol.playground/Contents.swift
@@ -4,7 +4,19 @@ import UIKit
  # Part 3: Protocol in Swift
  1. Declare a struct Person with a name property type String and a protocol name PoliceMan . There is only one method arrestCriminals with no argument in the protocol.
  */
+    struct Person: PoliceMan {
+        // 2
+        var name: String
+        // 4
+        var toolMan: ToolMan
+        func arrestCriminals() {
+        
+        }
+    }
 
+    protocol PoliceMan {
+        func arrestCriminals()
+    }
 /*:
  2. Make struct Person conform to PoliceMan protocol.
  */
@@ -12,7 +24,9 @@ import UIKit
 /*:
  3. Declare a protocol ToolMan with a method fixComputer that has no argument.
  */
-
+    protocol ToolMan {
+        func fixComputer()
+    }
 /*:
  4. Add a property toolMan to the struct Person with data type ToolMan .
  */
@@ -20,8 +34,13 @@ import UIKit
 /*:
  5. Declare a struct named Engineer that conforms to the ToolMan protocol.
  */
-
+    struct Engineer: ToolMan {
+        func fixComputer() {
+        
+        }
+    }
 /*:
  6. Create a Person instance with the name “Steven” and also create the relative data you need to declare this instance.
 
  */
+    let steven = Person(name: "Steven", toolMan: Engineer())
